fabf58ce5356a2710dbe357c1fc0515c16e21997
reymartvega@gmail.com <reymartvega@gmail.com>
2023-03-02 12:31:05 +0800

    Added routes for product

---
+++ src/scenes/products/index.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import { Box,Card,CardActions,CardContent,Collapse,Button,Typography,Rating,useTheme,useMediaQuery } from '@mui/system';
+import { useGetProductsQuery } from 'state/api';
+import Header from 'components/Header'
+const Products = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Products
