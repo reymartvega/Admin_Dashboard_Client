f1da0a0e530f2b50dc07890e91166fa5a29dad6e
reymartvega@gmail.com <reymartvega@gmail.com>
2023-03-05 11:02:04 +0800

    Added daily,monthly, and breakdown charts

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Customers from "scenes/customers"
 import Transactions from "scenes/transactions"
 import Geography from "scenes/geography"
 import Overview from "scenes/overview"
+import Daily from "scenes/daily"
+import Monthly from "scenes/monthly"
+import Breakdown from "scenes/breakdown"
 function App() {
   const mode = useSelector((state)=> state.global.mode)
   const theme = useMemo(()=> createTheme(themeSettings(mode)),[mode])
@@ -28,8 +31,11 @@ function App() {
               <Route path="/transactions" element={<Transactions/>}/>
               <Route path="/geography" element={<Geography/>}/>
               <Route path="/overview" element={<Overview/>}/>
+              <Route path="/daily" element={<Daily/>}/>
+              <Route path="/monthly" element={<Monthly/>}/>
+              <Route path="/breakdown" element={<Breakdown/>}/>
             </Route>
-          </Routes>
+          </Routes> 
         </ThemeProvider>
       </BrowserRouter>
     </div>
